48e04f19eee0395a2091f0fcec44df7369c6617e
LucianC2025 <lucian.cruz2025@gbwl.org>
2025-04-14 12:56:03 +0000

    skibdiidadasfas

--- main.py
+++ main.py
@@ -32,10 +32,17 @@ crops_df = pd.read_csv('crops.csv')
 
 app = Flask(__name__)
 
+# **************** ROUTES ******************
+# Home
 @app.route("/")
 @app.route("/home/")
 def index():
     return render_template('index.html')
+
+# NPCS
+@app.route('/routes/npc_routes/<route_type>')
+def npcroute(route_type): 
+    return render_template(f'routes/npc_routes/{route_type}.html', route_type=route_type)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = 1234, debug=True)
